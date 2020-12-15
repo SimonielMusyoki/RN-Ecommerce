4145749ae2122f32144a3493f7f9808003543f4e
SimonielMusyoki <musyoki.mtk3@gmail.com>
2020-12-15 14:02:32 +0300

    Assets Configuration

---
+++ constants/icons.js
@@ -0,0 +1,25 @@
+export const car_seats = require('../assets/icons/car-seats.png');
+export const car = require('../assets/icons/car.png');
+export const floor_mats = require('../assets/icons/floor-mats.png');
+export const fuels = require('../assets/icons/fuels.png');
+export const headlights = require('../assets/icons/headlights.png');
+export const motor_cycle = require('../assets/icons/motor-cycle.png');
+export const oils = require('../assets/icons/oils.png');
+export const rims = require('../assets/icons/rims.png');
+export const spare_parts = require('../assets/icons/spare-parts.png');
+export const steering_wheel = require('../assets/icons/steering-wheel.png');
+export const wheels = require('../assets/icons/wheels.png');
+
+export default {
+    car_seats,
+    car,
+    floor_mats,
+    fuels,
+    headlights,
+    motor_cycle,
+    oils,
+    rims,
+    spare_parts,
+    steering_wheel,
+    wheels
+};
---
+++ constants/images.js
@@ -0,0 +1,63 @@
+// Brands
+export const bmw = require('../assets/images/brands/bmw.svg')
+export const mercedes_benz = require('../assets/images/brands/mercedes-benz.svg')
+export const bughatti = require('../assets/images/brands/bughatti.jpg')
+export const ferari = require('../assets/images/brands/ferari.png')
+export const lamborghini = require('../assets/images/brands/lamboghini.png')
+export const tesla = require('../assets/images/brands/tesla.png')
+export const volkswagen = require('../assets/images/brands/volkswagen.png')
+
+// Sliders
+export const photo_cover_1 = require('../assets/images/sliders/photo-cover-1.jpeg')
+export const photo_cover_2 = require('../assets/images/sliders/photo-cover-2.jpeg')
+export const photo_cover_3 = require('../assets/images/sliders/photo-cover-3.jpeg')
+export const photo_cover_4 = require('../assets/images/sliders/photo-cover-4.jpeg')
+export const photo_cover_5 = require('../assets/images/sliders/photo-cover-5.jpeg')
+export const photo_cover_6 = require('../assets/images/sliders/photo-cover-6.jpeg')
+export const photo_cover_7 = require('../assets/images/sliders/photo-cover-7.jpeg')
+export const photo_cover_8 = require('../assets/images/sliders/photo-cover-8.jpeg')
+export const photo_cover_9 = require('../assets/images/sliders/photo-cover-9.jpeg')
+export const photo_cover_10 = require('../assets/images/sliders/photo-cover-10.jpeg')
+export const photo_cover_11 = require('../assets/images/sliders/photo-cover-11.jpeg');
+
+// Products
+export const product_1 = require('../assets/images/products/product-1.jpeg');
+export const product_2 = require('../assets/images/products/product-2.jpeg');
+export const product_3 = require('../assets/images/products/product-3.jpeg');
+export const product_4 = require('../assets/images/products/product-4.jpeg');
+export const product_5 = require('../assets/images/products/product-5.jpeg');
+export const product_6 = require('../assets/images/products/product-6.jpeg');
+export const product_7 = require('../assets/images/products/product-7.jpeg');
+export const product_8 = require('../assets/images/products/product-9.jpeg');
+
+export default {
+    bmw,
+    mercedes_benz,
+    bughatti,
+    ferari,
+    lamborghini,
+    tesla,
+    volkswagen,
+
+    photo_cover_1,
+    photo_cover_2,
+    photo_cover_3,
+    photo_cover_4,
+    photo_cover_5,
+    photo_cover_6,
+    photo_cover_7,
+    photo_cover_8,
+    photo_cover_9,
+    photo_cover_10,
+    photo_cover_11,
+
+    product_1,
+    product_2,
+    product_3,
+    product_4,
+    product_5,
+    product_6,
+    product_7,
+    product_8,
+
+};
